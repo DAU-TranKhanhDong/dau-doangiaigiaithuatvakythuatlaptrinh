1817f8f144208c292603ece9dfa63040e7287ea7
DAU-TranKhanhDong <dong_2451220033@dau.edu.vn>
2026-03-09 19:52:31 +0700

    Add files via upload

---
+++ Water Bottles.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        total = 0
+        empty = 0
+        
+        while numBottles > 0:
+            total += numBottles
+            empty += numBottles
+            numBottles = empty // numExchange
+            empty = empty % numExchange
+        
+        return total
